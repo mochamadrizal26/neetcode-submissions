007deefb9adb270204d40af58af505c6198d64e0
Mochamad Rizal Hidayat <39995309+mochamadrizal26@users.noreply.github.com>
2026-08-25 10:39:37 +0200

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int profit = 0;
+        for(int i=0; i<prices.length; i++) {
+            for(int j=i+1; j<prices.length; j++) {
+                profit = prices[j] - prices[i];
+                if(profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
